0f0de8ffad74e978fc1347836a3b2c8b9cb3501b
favour1818 <84434963+favour1818@users.noreply.github.com>
2023-04-03 19:43:03 +0100

    adding our dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://favour1818.github.io/QR-code-Generator',
   plugins: [react()],
 })
